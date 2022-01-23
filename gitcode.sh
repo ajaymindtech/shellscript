fc788e4c7fe55ffdcac2fb20b390f54ccd429596
ajaymindtech <ajaymindtech2021@gmail.com>
2022-01-23 13:19:07 +0530

    add the files to master

---
+++ gitcode.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# echo "# shellscript" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M master
+# git remote add origin https://github.com/ajaymindtech/shellscript.git
+# git push -u origin master
+
+echo " check the status"
+git status
+echo " add te files"
+git add .
+echo " commit the files"
+git commit -m 'add the files to master'
+echo " push to remote"
+git push 
+# echo " create the branch dev"
+# git branch dev
+# echo " push the branch"
+# git push -u origin dev
+
+
